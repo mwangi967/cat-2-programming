ea30771c455c6855d1c01647169cc2c026a54762
mwangi967 <mwangijonh2211@gmail.com>
2024-11-07 11:52:11 -0800

    Add files via upload

---
+++ array cat.c
@@ -0,0 +1,1 @@
+#include <stdio.h> int main() { int scores[2][2] = { {85, 90}, {78, 88} }; for (int i = 0; i < 2; i++) { for (int j = 0; j < 2; j++) { printf("scores[%d][%d] = %d\n", i, j, scores[i][j]); } } return 0;
